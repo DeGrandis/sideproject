7d7f83e2ba8ca2fb3585199b094369b221eeb1b1
DeGrandis <robd@vt.edu>
2025-06-27 22:21:23 -0400

    password issue

--- auth_backend/db_connection.py
+++ auth_backend/db_connection.py
@@ -7,7 +7,7 @@ import os
 from UserModel import User  # Import the User model from UserModel.py
 
 DATABASE_URL = f"postgresql://postgres:${os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST', 'localhost')}:5432/postgres"
-
+print(f"Connecting to database at {DATABASE_URL}")
 engine = create_engine(DATABASE_URL)
 
 # Create a configured "Session" class
